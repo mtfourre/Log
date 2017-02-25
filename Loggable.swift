04660d7103c6f0753bbf12f41bd40cf64f4235d9
Michael Fourre <mtfourre@gmail.com>
2017-02-24 23:10:56 -0800

    Adding public visibility to  function and adjusting internal function visibility to fileprivate

--- Loggable.swift
+++ Loggable.swift
@@ -1,17 +1,16 @@
 //
 //  Loggable.swift
-//  Dragon
+//  Loggable
 //
 //  Created by Michael Fourre on 2/4/17.
-//  Copyright © 2017 MealSloth. All rights reserved.
 //
 
-protocol Loggable {
+public protocol Loggable {
     
 }
 
-extension Loggable {
-    internal static func position(file: String, function: String, line: Int) -> String {
+public extension Loggable {
+    fileprivate static func position(file: String, function: String, line: Int) -> String {
         let path = file.components(separatedBy: "/")
         if let file = path[safe: path.count - 1]?.replacingOccurrences(of: ".swift", with: "") {
             return "<\(file):\(line)>"
@@ -19,11 +18,11 @@ extension Loggable {
         return ""
     }
     
-    internal static func logFirst(_ type: String, file: String, function: String, line: Int, strArr: [String]?) {
+    fileprivate static func logFirst(_ type: String, file: String, function: String, line: Int, strArr: [String]?) {
         print("Log.\(type)\(position(file: file, function: function, line: line)): \(strArr?.first ?? "Could not parse string")")
     }
     
-    internal static func logRemaining(_ strArr: [String]?) {
+    fileprivate static func logRemaining(_ strArr: [String]?) {
         guard let count = strArr?.count , count > 1 else { return }
         for newLine in 1..<count {
             if let line = strArr?[safe: newLine] {
@@ -32,7 +31,7 @@ extension Loggable {
         }
     }
     
-    internal static func log(type: String, string: String?, file: String, function: String, line: Int) {
+    public static func log(type: String, string: String?, file: String, function: String, line: Int) {
         let strArr = string?.components(separatedBy: "\n")
         self.logFirst(type, file: file, function: function, line: line, strArr: strArr)
         self.logRemaining(strArr)
