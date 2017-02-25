859636171d8a561214e76924bd80b136441f20dc
Michael Fourre <mtfourre@gmail.com>
2017-02-24 23:11:26 -0800

    Exposee Log API for use via pod

--- Log.swift
+++ Log.swift
@@ -5,8 +5,8 @@
 //  Created by Michael Fourre on 7/15/16.
 //
 
-struct Log: Loggable {
-    enum LogType: String {
+public struct Log: Loggable {
+    public enum LogType: String {
         case debug = "Debug"
         case error = "Error"
         case info = "Info"
@@ -15,27 +15,27 @@ struct Log: Loggable {
         case warning = "Warning"
     }
     
-    static func debug(_ string: String?, file: String = #file, function: String = #function, line: Int = #line) {
+    public static func debug(_ string: String?, file: String = #file, function: String = #function, line: Int = #line) {
         self.log(type: LogType.debug.rawValue, string: string, file: file, function: function, line: line)
     }
     
-    static func error(_ string: String?, file: String = #file, function: String = #function, line: Int = #line) {
+    public static func error(_ string: String?, file: String = #file, function: String = #function, line: Int = #line) {
         self.log(type: LogType.error.rawValue, string: string, file: file, function: function, line: line)
     }
     
-    static func info(_ string: String?, file: String = #file, function: String = #function, line: Int = #line) {
+    public static func info(_ string: String?, file: String = #file, function: String = #function, line: Int = #line) {
         self.log(type: LogType.info.rawValue, string: string, file: file, function: function, line: line)
     }
     
-    static func normal(_ string: String?, file: String = #file, function: String = #function, line: Int = #line) {
+    public static func normal(_ string: String?, file: String = #file, function: String = #function, line: Int = #line) {
         self.log(type: LogType.normal.rawValue, string: string, file: file, function: function, line: line)
     }
     
-    static func verbose(_ string: String?, file: String = #file, function: String = #function, line: Int = #line) {
+    public static func verbose(_ string: String?, file: String = #file, function: String = #function, line: Int = #line) {
         self.log(type: LogType.verbose.rawValue, string: string, file: file, function: function, line: line)
     }
     
-    static func warning(_ string: String?, file: String = #file, function: String = #function, line: Int = #line) {
+    public static func warning(_ string: String?, file: String = #file, function: String = #function, line: Int = #line) {
         self.log(type: LogType.warning.rawValue, string: string, file: file, function: function, line: line)
     }
 }
